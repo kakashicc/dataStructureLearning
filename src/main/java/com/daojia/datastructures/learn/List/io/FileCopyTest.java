5c7d10dcb0d6a0ebd2da76b37b0881c4e19b33e3
maosen <maosen@daojia-inc.com>
2019-11-05 21:09:46 +0800

    io

---
+++ src/main/java/com/daojia/datastructures/learn/List/io/FileCopyTest.java
@@ -0,0 +1,81 @@
+package com.daojia.datastructures.learn.List.io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * @Author: maosen
+ * @Description:
+ * @Date: Created in 2019/11/5 20:34.
+ */
+public class FileCopyTest {
+
+    public static void main(String[] args) {
+        String source = "C:\\Users\\maosen\\Desktop\\新建文件夹1";
+        String target = "C:\\Users\\maosen\\Desktop\\新建文件夹2";
+        try {
+            copyFolder(source,target);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 复制文件目录
+     * @param source 源路径
+     * @param target 目标路径
+     * @throws Exception
+     */
+    public static void copyFolder(String source,String target) throws Exception{
+
+        File sourceFile = new File(source);
+        File targetFile = new File(target,sourceFile.getName());
+        if(!targetFile.exists()){
+            targetFile.mkdir();
+        }
+
+        File[] files = sourceFile.listFiles();
+
+        for (File file : files) {
+            System.out.println("文件"+file.getName());
+            System.out.println("targetFile"+targetFile.getAbsolutePath());
+            if(!file.isDirectory()){
+                copyFile(file,new File(targetFile.getAbsolutePath(),file.getName()));
+            }else{
+                copyFolder(file.getAbsolutePath(),targetFile.getAbsolutePath());
+            }
+        }
+
+    }
+
+    /**
+     * 复制文件
+     * @param source 源文件
+     * @param target 目标文件
+     * @throws Exception
+     */
+    public static void copyFile(File source, File target) throws Exception{
+        FileInputStream fis = new FileInputStream(source);
+        //BufferedInputStream bis = new BufferedInputStream(fis);
+
+        FileOutputStream fos = new FileOutputStream(target);
+        //BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+        byte[] bytes = new byte[1024];
+        int len = 0;
+        while((len = fis.read(bytes)) != -1){
+            fos.write(bytes,0,len);
+        }
+
+        //bos.flush();
+
+        //bis.close();
+        //bos.close();
+
+        fis.close();
+        fos.close();
+    }
+}
---
+++ src/main/java/com/daojia/datastructures/learn/List/io/FileTest.java
@@ -0,0 +1,127 @@
+package com.daojia.datastructures.learn.List.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @Author: maosen
+ * @Description:
+ * @Date: Created in 2019/11/5 19:19.
+ */
+public class FileTest {
+
+    public static void main(String[] args) {
+        read();
+        read1();
+        copyFile();
+    }
+
+    public static void read() {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("C:\\Users\\maosen\\Desktop\\aa.txt");
+            int value;
+            value = fis.read();
+            while (value != -1) {
+                System.out.println(value);
+                value = fis.read();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void read1() {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("C:\\Users\\maosen\\Desktop\\aa.txt");
+            StringBuilder result = new StringBuilder();
+            int value;
+            value = fis.read();
+            while (value != -1) {
+                result.append((char)value);
+                value = fis.read();
+            }
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void read2() {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("C:\\Users\\maosen\\Desktop\\aa.txt");
+            StringBuilder result = new StringBuilder();
+            int value;
+            byte [] buffer = new byte[1024];
+            value = fis.read(buffer);
+            while (value != -1) {
+                result.append(new String(buffer,0,value));
+                value = fis.read(buffer);
+            }
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void copyFile(){
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            fis = new FileInputStream("C:\\Users\\maosen\\Desktop\\aa.txt");
+            fos = new FileOutputStream("C:\\Users\\maosen\\Desktop\\ab.txt");
+            StringBuilder result = new StringBuilder();
+            int value;
+            byte [] buffer = new byte[1];
+            value = fis.read(buffer);
+            while (value != -1) {
+                System.out.println(value);
+                System.out.println((char)buffer[0]);
+                fos.write(buffer,0,value);
+                value = fis.read(buffer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
