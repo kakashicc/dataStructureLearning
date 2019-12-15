5a3d74007dad894947e4849ccb47d8fbac69d7a0
kakashicc <3589343335@qq.com>
2019-12-15 12:37:35 +0800

    +） 单链表添加

--- src/main/java/com/daojia/datastructures/learn/PlainLock.java
+++ src/main/java/com/daojia/datastructures/learn/PlainLock.java
@@ -6,4 +6,9 @@ package com.daojia.datastructures.learn;
  * @Date: Created in 2019/12/4 21:37.
  */
 public class PlainLock {
+
+
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(new Object().hashCode()).length());
+    }
 }
